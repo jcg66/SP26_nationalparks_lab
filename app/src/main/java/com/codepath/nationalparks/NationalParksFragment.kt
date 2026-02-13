2428225b484bc4b23b7aeec7012a976fdf6307dc
James Girgis <231030483+jcg66@users.noreply.github.com>
2026-02-13 17:14:51 -0500

    stretch goal: add page control buttons to view more items

--- app/src/main/java/com/codepath/nationalparks/NationalParksFragment.kt
+++ app/src/main/java/com/codepath/nationalparks/NationalParksFragment.kt
@@ -5,6 +5,8 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.core.widget.ContentLoadingProgressBar
 import androidx.fragment.app.Fragment
@@ -30,6 +32,29 @@ private const val API_KEY = BuildConfig.API_KEY
 
  */
 class NationalParksFragment : Fragment(), OnListFragmentInteractionListener {
+    var totalItems: Int? = null
+    val pageLimit = 5
+    var pageNum: Int = 0
+    lateinit var prevPageButton: Button
+    lateinit var nextPageButton: Button
+    lateinit var pageNumText: TextView
+    lateinit var progressBar: ContentLoadingProgressBar
+    lateinit var recyclerView: RecyclerView
+    fun updatePageControls() {
+        if (totalItems == null) {
+            prevPageButton.isEnabled = false
+            nextPageButton.isEnabled = false
+        } else {
+            val totalPages = totalItems!! / pageLimit
+            pageNumText.text = "$pageNum / $totalPages"
+            prevPageButton.isEnabled = (pageNum > 0)
+            nextPageButton.isEnabled = (pageNum < totalPages)
+        }
+    }
+    fun updatePage(newNum: Int) {
+        pageNum = newNum
+        updateAdapter()
+    }
         /*
      * Constructing the view
      */
@@ -38,13 +63,19 @@ class NationalParksFragment : Fragment(), OnListFragmentInteractionListener {
             savedInstanceState: Bundle?
         ): View? {
             val view = inflater.inflate(R.layout.fragment_national_parks_list, container, false)
-            val progressBar = view.findViewById<View>(R.id.progress) as ContentLoadingProgressBar
-            val recyclerView = view.findViewById<View>(R.id.list) as RecyclerView
+            progressBar = view.findViewById<View>(R.id.progress) as ContentLoadingProgressBar
+            recyclerView = view.findViewById<View>(R.id.list) as RecyclerView
             val context = view.context
+
+            prevPageButton = view.findViewById<Button>(R.id.page_prev)
+            nextPageButton = view.findViewById<Button>(R.id.page_next)
+            pageNumText = view.findViewById<TextView>(R.id.page_num)
+            prevPageButton.setOnClickListener { updatePage(pageNum - 1) }
+            nextPageButton.setOnClickListener { updatePage(pageNum + 1) }
 
             recyclerView.layoutManager = LinearLayoutManager(context)
 
-            updateAdapter(progressBar, recyclerView)
+            updateAdapter()
             return view
         }
 
@@ -52,8 +83,11 @@ class NationalParksFragment : Fragment(), OnListFragmentInteractionListener {
      * Updates the RecyclerView adapter with new data.  This is where the
      * networking magic happens!
      */
-    private fun updateAdapter(progressBar: ContentLoadingProgressBar, recyclerView: RecyclerView) {
+    private fun updateAdapter() {
         progressBar.show()
+        // disable buttons while loading
+        prevPageButton.isEnabled = false
+        nextPageButton.isEnabled = false
 
         // Create and set up an AsyncHTTPClient() here
 
@@ -63,6 +97,8 @@ class NationalParksFragment : Fragment(), OnListFragmentInteractionListener {
         // Using the client, perform the HTTP request
 
         params["api_key"] = API_KEY
+        params["limit"] = pageLimit.toString()
+        params["start"] = (pageLimit * pageNum).toString()
         client["https://developer.nps.gov/api/v1/parks", params, object : JsonHttpResponseHandler()
         /* Uncomment me once you complete the above sections! */
         {
@@ -78,8 +114,13 @@ class NationalParksFragment : Fragment(), OnListFragmentInteractionListener {
                 // The wait for a response is over
                 progressBar.hide()
 
+                // update page controls using total
+                totalItems = json.jsonObject.getInt("total")
+                updatePageControls()
+
                 // Filter out the "data" JSON array and turn into a String
                 val dataJSON = json.jsonObject.get("data") as JSONArray
+
                 val parksRawJSON = dataJSON.toString()
 
                 // Create a Gson instance to help parse the raw JSON
@@ -91,6 +132,7 @@ class NationalParksFragment : Fragment(), OnListFragmentInteractionListener {
                 // Convert the raw JSON string into a list of actual NationalPark data models
                 val models: List<NationalPark> = gson.fromJson(parksRawJSON, arrayParkType)
 
+                recyclerView.swapAdapter(null,false) // clear old adapter (if any)
                 recyclerView.adapter = NationalParksRecyclerViewAdapter(models, this@NationalParksFragment)
 
                 // Look for this in Logcat:
